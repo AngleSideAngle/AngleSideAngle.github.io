0d3d2cea00d3ac489e4b5f3c621854e15cb94a61
Asa Paparo <77820164+AngleSideAngle@users.noreply.github.com>
2021-06-27 14:42:27 -0400

    projects is now auto generated

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,8 +1,27 @@
 
 const request = new XMLHttpRequest();
-request.open("GET", "https://api.github.com/users/AngleSideAngle");
+request.open("GET", "https://api.github.com/users/AngleSideAngle/repos");
 request.send();
 
+let projectBox = (title, content, link) => `<div><a href = "${link}"><h3>${title}</h3><p>${content}</p></a></div>`;
+
+const projects = document.getElementById("projects").getElementsByClassName("container")[0];
+
+addProject = (repo) => {
+    projects.innerHTML += projectBox(repo.name, repo.description, repo.html_url);
+}
+
 request.onload = () => {
-    console.log(request);
+    if(request.status === 200) {
+        response = JSON.parse(request.response);
+        for(repo of response) {
+            if(repo.description) {
+                addProject(repo);
+                console.log(repo)
+            }
+        }
+    } else {
+        console.log(request)
+        console.log(`error: ${request.status}`)
+    }
 }
