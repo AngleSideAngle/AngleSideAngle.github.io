35a7fd4743d2b3c9ddd3cff92b5e930204fb6993
Asa Paparo <asapaparo@gmail.com>
2025-03-28 01:57:25 -0400

    more cooking

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,4 @@
 
 export const SITE_TITLE = 'Asa Paparo';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SOURCE_URL = 'https://github.com/AngleSideAngle/AngleSideAngle.github.io/tree/main/';
